7e5788ab1f4b206ae260351e688e1666a0ad98d9
Donice Ubaru <donice.ubaru@Donices-MacBook-Pro.local>
2026-05-10 07:05:04 +0100

    Perfected the email for waitlist

--- src/components/common/logo/index.tsx
+++ src/components/common/logo/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import LogoImage from "@/assets/logo_adaptations/android-chrome-512x512.png"
+import LogoImage from "@/assets/logo_adaptations/full.png"
 
 const Logo = ({}) => {
   return (
     <Link href={"/"} className='flex items-center gap-1'>
-      <Image src={LogoImage} alt='Logo' className='w-8 md:w-11' />
-      <span className='text-3xl tracking-tighter font-medium'>Clap</span>
+      <Image src={LogoImage} alt='Logo' className='w-20 md:w-22' />
     </Link>
   )
 }
